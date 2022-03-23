ad0634d530c67ce66b49180dee6b65656be585e4
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-03-23 15:51:52 +0100

    Fix transparency problem

--- srcs/bresenham2.c
+++ srcs/bresenham2.c
@@ -6,7 +6,7 @@
 /*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:00:15 by amuhleth          #+#    #+#             */
-/*   Updated: 2022/03/04 20:41:43 by amuhleth         ###   ########.fr       */
+/*   Updated: 2022/03/23 15:50:00 by amuhleth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	line_q1(t_fdf *a, t_2d p1, t_2d p2)
 	i = 0;
 	while (p1.x <= p2.x)
 	{
-		put_pixel_to_img(&a->i, p1.x, p1.y, 0xFFFFFFFF);
+		put_pixel_to_img(&a->i, p1.x, p1.y, 0x00FFFFFF);
 		p1.x++;
 		if (p < 0)
 			p = p + 2 * dy;
@@ -50,7 +50,7 @@ void	line_q2(t_fdf *a, t_2d p1, t_2d p2)
 	i = 0;
 	while (p1.y <= p2.y)
 	{
-		put_pixel_to_img(&a->i, p1.x, p1.y, 0xFFFFFFFF);
+		put_pixel_to_img(&a->i, p1.x, p1.y, 0x00FFFFFF);
 		p1.y++;
 		if (p < 0)
 			p = p + 2 * dx;
@@ -75,7 +75,7 @@ void	line_q3(t_fdf *a, t_2d p1, t_2d p2)
 	i = 0;
 	while (p1.y <= p2.y)
 	{
-		put_pixel_to_img(&a->i, p1.x, p1.y, 0xFFFFFFFF);
+		put_pixel_to_img(&a->i, p1.x, p1.y, 0x00FFFFFF);
 		p1.y++;
 		if (p < 0)
 			p = p + 2 * dx;
@@ -100,7 +100,7 @@ void	line_q4(t_fdf *a, t_2d p1, t_2d p2)
 	i = 0;
 	while (p1.x >= p2.x)
 	{
-		put_pixel_to_img(&a->i, p1.x, p1.y, 0xFFFFFFFF);
+		put_pixel_to_img(&a->i, p1.x, p1.y, 0x00FFFFFF);
 		p1.x--;
 		if (p < 0)
 			p = p + 2 * dy;
